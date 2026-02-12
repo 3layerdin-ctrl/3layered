5e92a90f8bf05daa1e873603cf155ad136f0a934
makeathera-byte <makeathera@gmail.com>
2026-02-13 00:53:13 +0530

    Add Meta Pixel tracking (FB Pixel ID 936978879005002)

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,8 @@ import { Footer } from "@/components/Footer";
 import { WhatsAppButton } from "@/components/WhatsAppButton";
 import { Analytics } from "@vercel/analytics/react";
 import { BlogJsonLd, OrganizationJsonLd, WebSiteJsonLd } from "@/components/StructuredData";
+import Script from "next/script";
+import MetaPixel from "./meta-pixel";
 
 const playfair = Playfair_Display({
   variable: "--font-playfair",
@@ -97,9 +99,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        <Script
+          id="meta-pixel-base"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '936978879005002');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${inter.variable} ${playfair.variable} antialiased`}
       >
+        <MetaPixel />
         <AuthProvider>
           <SlideProvider>
             <LogoController />
