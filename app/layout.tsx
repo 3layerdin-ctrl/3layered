a341b034884a963f01f1f50c947893100bd61d36
makeathera-byte <makeathera@gmail.com>
2026-02-18 18:45:57 +0530

    fix: remove duplicate Meta Pixel PageView tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -116,7 +116,6 @@ export default function RootLayout({
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
               fbq('init', '936978879005002');
-              fbq('track', 'PageView');
             `,
           }}
         />
