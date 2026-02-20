b98483338f4eaad56b5a41a491f3f9f59fe665cb
makeathera-byte <makeathera@gmail.com>
2026-02-20 19:26:01 +0530

    fix: switch GTM to official @next/third-parties package for reliable tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { Footer } from "@/components/Footer";
 import { WhatsAppButton } from "@/components/WhatsAppButton";
 import { Analytics } from "@vercel/analytics/react";
 import { BlogJsonLd, OrganizationJsonLd, WebSiteJsonLd } from "@/components/StructuredData";
-import Script from "next/script";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 
 const playfair = Playfair_Display({
@@ -99,26 +99,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
-      <head>
-        {/* Google Tag Manager */}
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-WMPHN6CZ');
-            `,
-          }}
-        />
-      </head>
+      <GoogleTagManager gtmId="GTM-WMPHN6CZ" />
       <body
         className={`${inter.variable} ${playfair.variable} antialiased`}
       >
-        {/* Google Tag Manager (noscript fallback) */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-WMPHN6CZ"
